eaf45d11285123c8d806843200a527c97859b7cd
Lazar <lazar.vujovic1993@gmail.com>
2025-08-08 14:02:59 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="text-2xl flex justify-center pt-20">
-      Web stranica u izradi - Dubocica
+      Web stranica u izradi - Dubocica test
     </div>
   );
 }
