6a1693ad01bb27b5a837ea6263e39d7ab450466e
Lazar <lazar.vujovic1993@gmail.com>
2025-08-08 18:21:38 +0200

    test

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -1,17 +1,20 @@
-// app/api/test-db/route.ts
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const user = await prisma.user.upsert({
-      where: { email: 'test@example.com' },
-      update: {}, // ništa ne menja ako postoji
-      create: { email: 'test@example.com', name: 'Test User' },
+    // upišemo jednog usera
+    await prisma.user.create({
+      data: { email: 'test@example.com', name: 'Test User' },
     });
-    return NextResponse.json({ ok: true, user });
-  } catch (e: any) {
-    return NextResponse.json({ ok: false, error: e.message }, { status: 500 });
+
+    // pročitamo sve usere
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ success: true, users });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ success: false, error }, { status: 500 });
   }
 }
