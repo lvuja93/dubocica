2a59c51047b5c75f36ac1e2a98380c0a029508f0
Lazar <lazar.vujovic1993@gmail.com>
2025-08-09 13:20:53 +0200

    u izradi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="text-2xl flex justify-center pt-20">
-      Web stranica u izradi - Dubocica test
+    <div className="text-2xl flex-1 justify-center pt-20 text-center">
+      <h1>
+        {' '}
+        Naselje Dubočica <br />
+        Web stranica u izradi{' '}
+      </h1>
+      <div>
+        <img src="./images/logo-dub.png" alt="" className="w-auto" />
+      </div>
     </div>
   );
 }
